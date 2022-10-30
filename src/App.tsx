01cf903cd3b87f3f125d8a12803ca6795206e746
misshu1 <nedelusandrei@gmail.com>
2022-10-30 16:01:42 +0200

    added setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { Button } from '@mui/material';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [token, setToken] = useState('');
+
+    useEffect(() => {
+        const hash = window.location.hash;
+        let token = window.localStorage.getItem('token') ?? '';
+
+        if (!token && hash !== undefined) {
+            const accessToken = hash
+                .substring(1)
+                .split('&')
+                .find((elem) => elem.startsWith('access_token'));
+            if (accessToken) {
+                token = accessToken.split('=')[1];
+            }
+
+            window.location.hash = '';
+            window.localStorage.setItem('token', token);
+        }
+
+        setToken(token);
+    }, []);
+
+    const logout = () => {
+        setToken('');
+        window.localStorage.removeItem('token');
+    };
+
+    return (
+        <div>
+            {!token ? (
+                <Button
+                    href={`${process.env.REACT_APP_SPOTIFY_AUTH_ENDPOINT}?client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_SPOTIFY_REDIRECT_URI}&response_type=${process.env.REACT_APP_SPOTIFY_RESPONSE_TYPE}`}
+                >
+                    Login
+                </Button>
+            ) : (
+                <Button onClick={logout}>Logout</Button>
+            )}
+        </div>
+    );
 }
 
 export default App;
